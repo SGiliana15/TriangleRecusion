96d4d4a19259ecd9d2787374963b35614d804e1a
Sean <zerog133@yahoo.com>
2025-07-09 20:52:04 -0500

    completed

---
+++ app/src/main/java/org/example/TriangleNumberCalculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class TriangleNumberCalculator {
+   
+    public int value(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return n + value(n - 1);
+    }
+
+      public int add(int n, int m) {
+        return value(n) + value(m);
+    }
+
+    public int subtract(int n, int m) {
+        return value(n) - value(m);
+    }
+}
